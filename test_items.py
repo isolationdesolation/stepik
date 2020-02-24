c7eb33efef27ec29dbd86c22bb5f7b5a3f5d1899
isolationdesolation <60424405+isolationdesolation@users.noreply.github.com>
2020-02-24 17:53:41 +0500

    Updated items test
    Added assertion for cart button

--- test_items.py
+++ test_items.py
@@ -6,4 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_cart_button(browser):
     browser.get(link)
     time.sleep(5)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), \
+        "Не найдена кнопка добавления товара в корзину"
