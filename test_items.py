5b018d645008b54e7a041eef776a27174b523369
isolationdesolation <60424405+isolationdesolation@users.noreply.github.com>
2020-02-24 17:50:30 +0500

    added solution for 9th step in 3.6 lesson
    just two files to make it work

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_cart_button(browser):
+    browser.get(link)
+    time.sleep(5)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
